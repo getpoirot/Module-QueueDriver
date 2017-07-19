2b71d8132d4f0cb433e79dde2fe5c0a5cfcb293e
Payam Naderi <naderi.payam@gmail.com>
2017-07-19 14:42:28 +0430

    missing files

--- src/Module.php
+++ src/Module.php
@@ -1,23 +1,15 @@
 <?php
-namespace Module\OAuth2
+namespace Module\QueueDriver
 {
     use Module\OAuth2\Services\BuildServices;
 
-    use Poirot\Application\aSapi;
-    use Poirot\Application\Interfaces\iApplication;
     use Poirot\Application\Interfaces\Sapi\iSapiModule;
-    use Poirot\Application\ModuleManager\Interfaces\iModuleManager;
     use Poirot\Application\Interfaces\Sapi;
     use Poirot\Application\Sapi\Module\ContainerForFeatureActions;
 
     use Poirot\Ioc\Container;
     use Poirot\Ioc\Container\BuildContainer;
 
-    use Poirot\Loader\Autoloader\LoaderAutoloadAggregate;
-    use Poirot\Loader\Autoloader\LoaderAutoloadNamespace;
-    use Poirot\Loader\Interfaces\iLoaderAutoload;
-
-    use Poirot\Loader\LoaderNamespaceStack;
     use Poirot\Router\BuildRouterStack;
     use Poirot\Router\Interfaces\iRouterStack;
 
@@ -25,87 +17,13 @@ namespace Module\OAuth2
 
 
     class Module implements iSapiModule
-        , Sapi\Module\Feature\iFeatureModuleInitSapi
-        , Sapi\Module\Feature\iFeatureModuleAutoload
-        , Sapi\Module\Feature\iFeatureModuleInitModuleManager
         , Sapi\Module\Feature\iFeatureModuleMergeConfig
         , Sapi\Module\Feature\iFeatureModuleNestActions
         , Sapi\Module\Feature\iFeatureModuleNestServices
         , Sapi\Module\Feature\iFeatureOnPostLoadModulesGrabServices
     {
-        const CONF_KEY      = 'module.oauth2';
-        const REALM = 'module.oauth2.default_authenticator';
+        const CONF = 'mod.queue_driver';
 
-
-        /**
-         * Init Module Against Application
-         *
-         * - determine sapi server, cli or http
-         *
-         * priority: 1000 A
-         *
-         * @param iApplication|aSapi $sapi Application Instance
-         *
-         * @return false|null False mean not setup with other module features (skip module)
-         * @throws \Exception
-         */
-        function initialize($sapi)
-        {
-            if ( \Poirot\isCommandLine( $sapi->getSapiName() ) )
-                // Sapi Is Not HTTP. SKIP Module Load!!
-                return false;
-        }
-
-        /**
-         * Register class autoload on Autoload
-         *
-         * priority: 1000 B
-         *
-         * @param LoaderAutoloadAggregate $baseAutoloader
-         *
-         * @return iLoaderAutoload|array|\Traversable|void
-         */
-        function initAutoload(LoaderAutoloadAggregate $baseAutoloader)
-        {
-            #$nameSpaceLoader = \Poirot\Loader\Autoloader\LoaderAutoloadNamespace::class;
-            $nameSpaceLoader = 'Poirot\Loader\Autoloader\LoaderAutoloadNamespace';
-            /** @var LoaderAutoloadNamespace $nameSpaceLoader */
-            $nameSpaceLoader = $baseAutoloader->loader($nameSpaceLoader);
-            $nameSpaceLoader->addResource(__NAMESPACE__, __DIR__);
-
-            require_once __DIR__.'/_functions.php';
-        }
-
-        /**
-         * Initialize Module Manager
-         *
-         * priority: 1000 C
-         *
-         * @param iModuleManager $moduleManager
-         *
-         * @return void
-         */
-        function initModuleManager(iModuleManager $moduleManager)
-        {
-            // ( ! ) ORDER IS MANDATORY
-
-            if (!$moduleManager->hasLoaded('MongoDriver'))
-                // MongoDriver Module Is Required.
-                $moduleManager->loadModule('MongoDriver');
-
-            if (!$moduleManager->hasLoaded('Authorization'))
-                // Authorization Module Is Required.
-                $moduleManager->loadModule('Authorization');
-
-            if (!$moduleManager->hasLoaded('OAuth2Client'))
-                // Load OAuth2 Client To Assert Tokens.
-                $moduleManager->loadModule('OAuth2Client');
-
-            if (!$moduleManager->hasLoaded('SmsClients'))
-                // SMS Client Module Is Required.
-                $moduleManager->loadModule('SmsClients');
-
-        }
 
         /**
          * Register config key/value
@@ -121,7 +39,7 @@ namespace Module\OAuth2
          */
         function initConfig(iDataEntity $config)
         {
-            return \Poirot\Config\load(__DIR__ . '/../../config/mod-oauth2_server');
+            return \Poirot\Config\load(__DIR__ . '/../config/mod-queue_driver');
         }
 
         /**
@@ -138,7 +56,7 @@ namespace Module\OAuth2
          */
         function getServices(Container $moduleContainer = null)
         {
-            $conf    = \Poirot\Config\load(__DIR__ . '/../../config/mod-oauth2_server_services');
+            $conf    = \Poirot\Config\load(__DIR__ . '/../config/mod-queue_driver_services');
 
             $builder = new BuildServices;
             $builder->with($builder::parseWith($conf));
@@ -156,7 +74,7 @@ namespace Module\OAuth2
          */
         function getActions()
         {
-            return \Poirot\Config\load(__DIR__ . '/../../config/mod-oauth2_server_actions');
+            return \Poirot\Config\load(__DIR__ . '/../config/mod-queue_driver_actions');
         }
 
         /**
@@ -172,23 +90,11 @@ namespace Module\OAuth2
          *
          * @internal param null $services service names must have default value
          */
-        function resolveRegisteredServices($viewModelResolver = null, $router = null)
+        function resolveRegisteredServices($router = null)
         {
-            # Attach Module Scripts To View Resolver:
-
-            // But We May Need Template Rendering Even In API Calls
-            /** @var LoaderNamespaceStack $resolver */
-            $resolver = $viewModelResolver->loader(LoaderNamespaceStack::class);
-            $resolver->with([
-                // Use Default Theme Folder To Achieve Views
-                'main/oauth/' => __DIR__. '/../../view/main/oauth',
-                'error/oauth/' => __DIR__ . '/../../view/error/oauth', // Looks for Errors In This Folder
-            ]);
-
-
             # Register Http Routes:
             if ($router) {
-                $routes = include __DIR__ . '/../../config/mod-oauth2_server_routes.conf.php';
+                $routes = include __DIR__ . '/../config/mod-queue_driver_routes.conf.php';
                 $buildRoute = new BuildRouterStack();
                 $buildRoute->setRoutes($routes);
                 $buildRoute->build($router);
@@ -198,64 +104,13 @@ namespace Module\OAuth2
 }
 
 
-namespace Module\OAuth2\Actions
+namespace Module\QueueDriver
 {
-
-    use Module\OAuth2\Actions\Recover\SigninChallengePage;
-    use Module\OAuth2\Actions\Recover\SigninNewPassPage;
-    use Module\OAuth2\Actions\Recover\SigninRecognizePage;
-    use Module\OAuth2\Actions\User\LoginPage;
-    use Module\OAuth2\Actions\User\LogoutPage;
-    use Module\OAuth2\Actions\User\RegisterPage;
-    use Module\OAuth2\Actions\User\RetrieveAuthenticatedUser;
-    use Module\OAuth2\Actions\Validation\ValidatePage;
-    use Module\OAuth2\Actions\Validation\ResendAuthCodeRequest;
-    use Module\OAuth2\Actions\Validation\Validation;
-    use Module\OAuth2\Model\Entity\UserEntity;
-
+    use Module\QueueDriver\Services\ContainerQueuesCapped;
 
     /**
-     * @property RegisterPage                   $RegisterPage
-     * @property LoginPage                      $LoginPage
-     * @property LogoutPage                     $LogoutPage
-     * @property ValidatePage                   $ValidatePage
-     * @property SigninRecognizePage            $SigninRecognizePage
-     * @property SigninChallengePage            $SigninChallengePage
-     * @property SigninNewPassPage              $SigninNewPassPage
-     * @property ResendAuthCodeRequest          $ResendAuthCodeRequest
-     * @property RetrieveAuthenticatedUser      $RetrieveAuthenticatedUser
-     *
-     * @method static UserEntity  RetrieveAuthenticatedUser()
-     * @method static Validation  Validation()
+     * @method static ContainerQueuesCapped Queues()
      */
-    class IOC extends \IOC
-    { }
-}
-
-namespace Module\OAuth2\Services
-{
-    /**
-     */
-    class IOC extends \IOC
-    { }
-}
-
-namespace Module\OAuth2\Services\Repository
-{
-    use Module\OAuth2\Interfaces\Model\Repo\iRepoUsers;
-    use Poirot\OAuth2\Interfaces\Server\Repository\iRepoAccessTokens;
-    use Poirot\OAuth2\Interfaces\Server\Repository\iRepoAuthCodes;
-    use Poirot\OAuth2\Interfaces\Server\Repository\iRepoClients;
-    use Poirot\OAuth2\Interfaces\Server\Repository\iRepoRefreshTokens;
-
-
-    /**
-     * @method static iRepoClients       Clients(array $options=null)
-     * @method static iRepoUsers         Users(array $options=null)
-     * @method static iRepoAccessTokens  AccessTokens(array $options=null)
-     * @method static iRepoRefreshTokens RefreshTokens(array $options=null)
-     * @method static iRepoAuthCodes     AuthCodes(array $options=null)
-     */
-    class IOC extends \IOC
+    class Services extends \IOC
     { }
 }
